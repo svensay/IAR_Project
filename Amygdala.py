fe5ed3c5b0c7af5a1e0d5ba7eca00a94f89c0703
svensay <svensay@gmail.com>
2019-11-13 13:14:37 +0100

    Ce qu'a fait Morgane

---
+++ Amygdala.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+class Amygdala:
+    def __init__(self,S,Th,rew) :
+        self.S = S
+        self.Th = Th
+        self.rew = rew
+        self.V = np.zeros(S.size+1)
+        self.Aj = 0
+        self.A = np.zeros(S.size+1)
+        
+    def calcul_somme_A(self):
+        self.Aj = np.sum(self.A)
+
+    def poids_Vi(self,alpha = 0.2) :
+        for i in range(self.V.size-1) :
+            self.V[i]+= alpha*(self.S[i]*max(0,(self.rew-self.Aj)))
+        self.V[-1]+=alpha*(self.Th*max(0,(self.rew-self.Aj)))
+
+    def maj_A(self):
+        for i in range(self.A.size-1):
+            self.A[i] = self.S[i] * self.V[i]
+        self.A[-1] = self.Th * self.V[-1]
+
+    def pas_de_temps(self,S,Th,rew) :
+        self.S = S
+        self.Th = Th
+        self.rew = rew
+        self.V.resize(S.size+1)
+        self.A.resize(S.size+1)
+        self.calcul_somme_A()
+        self.poids_Vi()
+        self.maj_A()
+
+    def calcul_E(self,O) :
+        return np.sum(self.A) - np.sum(O)
---
+++ CortexOrbitoFrontal.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+class CortexOrbitoFrontal :
+    def __init__(self,S,rew) :
+        self.S = S
+        self.rew = rew
+        self.W = np.zeros(S.size)
+        self.O = np.zeros(S.size)
+        self.Oj = 0
+
+    def calcul_somme_O(self) :
+        self.Oj = 0
+        for i in range(self.O.size) :
+            self.Oj+=self.O[i]-self.rew
+
+    def poids_Wi(self,beta = 0.8) :
+        for i in range(self.W.size) :
+            self.W[i]+=beta*self.S[i]*self.Oj
+
+    def maj_O(self):
+        for i in range(self.O.size) :
+            self.O[i] = self.S[i] * self.W[i]
+            
+    def pas_de_temps(self,S,rew) :
+        self.S = S
+        self.rew = rew
+        self.W.resize(S.size)
+        self.O.resize(S.size)
+        self.calcul_somme_O()
+        self.poids_Wi()
+        self.maj_O
